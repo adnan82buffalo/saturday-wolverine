5b8491acd048369b43d0f82b76608f09bd21ec1d
adnan82buffalo <adnan.ferdous.82@gmail.com>
2022-09-03 16:39:16 -0400

    fisrt commit

---
+++ src/com/upskill/java_1/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.upskill.java_1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World ! this is ADNAN");
+
+	}
+
+}
---
+++ src/com/upskill/java_4/array.java
@@ -0,0 +1,22 @@
+package com.upskill.java_4;
+
+public class array {
+
+	public static void main(String[] args) {
+	
+		// Array store multipul data using index
+		
+		int age = 34;                                             // Variable
+		
+		int[] ageWolverine = new int []{24, 28, 30, 35, 29, 40};  // Array
+		
+		//Array Index                   [0] [1] [2] [3] [4] [5]
+		
+		System.out.println("Student Age : " + ageWolverine[5]);
+		System.out.println("Total Student : " + ageWolverine.length);
+		
+		String[] nameWolverine = new String[]{"Ansarul", "Liaqath", "Sheakh", "Omar", "Rushdi", "Adnan"};
+		System.out.println(Student Name : " + nameWolverine[2]);
+	}
+
+}
